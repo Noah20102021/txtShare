d53464d01470b3bd1f64f1286894b045c0736675
Noah20102021 <schledernoah0@gmail.com>
2026-01-27 21:15:27 +0100

    eddeted automated domain configuration

--- src/main/java/com/kwirll/txtshare/config.java
+++ src/main/java/com/kwirll/txtshare/config.java
@@ -1,18 +1,6 @@
 package com.kwirll.txtshare;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 public class config {
 
-    public static String host;
-
-    static {
-        try {
-            String ip = InetAddress.getLocalHost().getHostAddress();
-            host = "http://" + ip + ":8080";
-        } catch (UnknownHostException e) {
-            host = "http://localhost:8080";
-        }
-    }
+    public static String host = "http://kwirll.ddnss.de:8080";
 }
